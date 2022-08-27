69451f463e4fea78d0ddd948c0b5dcc7da57cb67
gitlinux12 <mshirole94@gmail.com>
2022-08-27 17:29:59 +0530

    Java Logical_Programs

---
+++ Logical_Programs/src/Fibonacci/Fibonacci.java
@@ -0,0 +1,34 @@
+package Fibonacci;
+import java.util.*;
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		System.out.println(" Febonacci Series Program");
+
+		System.out.print("Enter number: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		if (number < 0) {
+			number = -number;
+		}
+		
+		System.out.println("You enterd "+number);
+		
+		int number1 = 0;
+		int number2 = 1;
+		if (number >= 1) System.out.println(number1);
+		if (number >= 2) System.out.println(number2);
+		
+		int sum;
+		while (number > 2) {
+			sum = number1 + number2;
+			number1 = number2;
+			number2 = sum;
+			System.out.println(sum);
+			number--;
+		}
+		
+		scanner.close();
+	}
+
+}
